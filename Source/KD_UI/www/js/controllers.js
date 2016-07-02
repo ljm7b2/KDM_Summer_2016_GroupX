dbfe5c97a69bc30c70e23598612e30cedc98f7ac
Luke McDuff <ljm7b2@mail.umkc.edu>
2016-07-01 21:53:03 -0500

    changed template style

---
+++ Source/KD_UI/www/js/controllers.js
@@ -0,0 +1,14 @@
+angular.module('app.controllers', [])
+  
+.controller('homeCtrl', function($scope) {
+
+})
+   
+.controller('aboutCtrl', function($scope) {
+
+})
+   
+.controller('contactCtrl', function($scope) {
+
+})
+    
